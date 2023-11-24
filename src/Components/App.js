013f3bcf38509e68dbb14ec16ac9685d8b477338
Wandana23 <wandanamuthumali99@gmail.com>
2023-11-24 07:08:48 +0530

    Develop an 'Add Contact' form for user input

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      "name": "Ryan",
-      "email":"rfp@gmail.com"
-    },
-    {
-      id: "2",
-      "name": "Nick",
-      "email":"nicks@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  const AddContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact])
+  };
   return (
     <div className="ui container">
       <Header />
-      <AddContact/>
+      <AddContact AddContactHandler={AddContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
