8eaba786a0dd6b7d2b2b36d610d94ffecb4ee03c
Wandana23 <wandanamuthumali99@gmail.com>
2023-11-26 10:45:42 +0530

    Update App.js with new functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const AddContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact])
   };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log('Retrived Contacts:', retriveContacts);
+    if (retriveContacts) setContacts(retriveContacts);
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
+
   return (
     <div className="ui container">
       <Header />
